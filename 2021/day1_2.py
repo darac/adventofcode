16271145c5689bfffef285449b5bffec18826148
Paul Saunders <p.saunders@cgi.com>
2021-12-03 09:36:03 +0000

    Linted all the code

--- 2021/day1_2.py
+++ 2021/day1_2.py
@@ -1,9 +1,53 @@
 #!env python3
+"""
+Considering every single measurement isn't as useful as you expected:
+there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window.
+Again considering the above example:
+
+199  A
+200  A B
+208  A B C
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G
+269    F G H
+260      G H
+263        H
+
+Start by comparing the first and second three-measurement windows.
+The measurements in the first window are marked A (199, 200, 208);
+their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210);
+its sum is 618. The sum of measurements in the second window is larger than
+the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements
+in this sliding window increases from the previous sum. So, compare A with B,
+then compare B with C, then C with D, and so on. Stop when there aren't enough
+measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window.
+How many sums are larger than the previous sum?
+"""
 
 from aocd.models import Puzzle
 from rich import print
 from rich.progress import track
-from collections import deque
 
 A_UPPERCASE = ord("A")
 ALPHABET_SIZE = 26
@@ -38,14 +82,25 @@ def base_alphabet_to_10(letters):
 
 
 class ThreeMeasurementWindow:
+    """
+    Holds the first three Measurements added.
+    When asked, will return the sum of the measurements held.
+    """
     def __init__(self) -> None:
         self.measurements = []
 
-    def measure(self, value: int):
+    def measure(self, val: int):
+        """
+        Takes a value and, if less than three values have been stored,
+        stores it. Else discards it.
+        """
         if len(self.measurements) < 3:
-            self.measurements.append(value)
+            self.measurements.append(val)
 
     def sum(self) -> int:
+        """
+        Returns the sum of values held, but only if three are held.
+        """
         if len(self.measurements) == 3:
             return sum(self.measurements)
 
@@ -57,19 +112,19 @@ for depth in track([int(n) for n in Puzzle(year=2021, day=1).input_data.splitlin
         window.measure(depth)
 
 
-prev = None
-count = 0
+PREV = None
+COUNT = 0
 for winnum, window in enumerate(windows):
-    name = base_10_to_alphabet(winnum)
+    NAME = base_10_to_alphabet(winnum)
     value = window.sum()
-    if prev is not None and value is not None:
-        if value > prev:
-            print(f"{name}: {value} ([bold]increased[/bold])")
-            count += 1
-        elif value < prev:
-            print(f"{name}: {value} ([bold]decreased[/bold])")
+    if PREV is not None and value is not None:
+        if value > PREV:
+            print(f"{NAME}: {value} ([bold]increased[/bold])")
+            COUNT += 1
+        elif value < PREV:
+            print(f"{NAME}: {value} ([bold]decreased[/bold])")
         else:
-            print(f"{name}: {value} ([bold]no change[/bold])")
-    prev = value
+            print(f"{NAME}: {value} ([bold]no change[/bold])")
+    PREV = value
 
-print(f"count = {count}")
+print(f"count = {COUNT}")
--- 2021/day2_1.py
+++ 2021/day2_1.py
@@ -1,32 +1,72 @@
 #!env python3
+"""
+Now, you need to figure out how to pilot this thing.
+
+It seems like the submarine can take a series of commands like
+forward 1, down 2, or up 3:
+
+    forward X increases the horizontal position by X units.
+    down X increases the depth by X units.
+    up X decreases the depth by X units.
+
+Note that since you're on a submarine, down and up affect your depth,
+and so they have the opposite result of what you might expect.
+
+The submarine seems to already have a planned course (your puzzle input).
+You should probably figure out where it's going. For example:
+
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+
+Your horizontal position and depth both start at 0.
+The steps above would then modify them as follows:
+
+    forward 5 adds 5 to your horizontal position, a total of 5.
+    down 5 adds 5 to your depth, resulting in a value of 5.
+    forward 8 adds 8 to your horizontal position, a total of 13.
+    up 3 decreases your depth by 3, resulting in a value of 2.
+    down 8 adds 8 to your depth, resulting in a value of 10.
+    forward 2 adds 2 to your horizontal position, a total of 15.
+
+After following these instructions, you would have a horizontal
+position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+Calculate the horizontal position and depth you would have after
+following the planned course. What do you get if you multiply your
+final horizontal position by your final depth?
+"""
 
 from aocd.models import Puzzle
 from rich import print
 
-print(f"[green]Captain:[/green] Helmsman! Prepare to recieve orders!")
+print("[green]Captain:[/green] Helmsman! Prepare to recieve orders!")
 
-position = 0
-depth = 0
+POSITION = 0
+DEPTH = 0
 
 for command, count in [n.split() for n in Puzzle(year=2021,day=2).input_data.splitlines()]:
     print(f"[green]Captain:[/green] {command} {count}")
     match command:
         case "forward":
             print(f"[green]Helmsman:[/green] Aye-aye, sir. Moving Forward {count}")
-            position += int(count)
+            POSITION += int(count)
         case "up":
             print(f"[green]Helmsman:[/green] Aye-aye, sir. Coming Up {count}")
-            depth -= int(count)
-            if depth == 0:
+            DEPTH -= int(count)
+            if DEPTH == 0:
                 print("[blue]Computer:[/blue] Surfaced")
-            elif depth < 0:
+            elif DEPTH < 0:
                 print("[blue]Computer:[/blue] FLYING!")
         case "down":
             print(f"[green]Helmsman:[/green] Aye-aye, sir. Going Down {count}")
-            depth += int(count)
+            DEPTH += int(count)
         case _:
             print(f"[green]Captain:[/green] Belay that {command} order!")
 
-print(f"[green]Captain:[/green] Helmsman, Report Position!")
-print(f"[green]Helmsman:[/green] Sir! Forward {position}, Depth {depth}, Sir!")
-print(f"[green]Captain:[/green] Thank you, sailor. Yeoman, record that as {position * depth}!")
+print("[green]Captain:[/green] Helmsman, Report Position!")
+print(f"[green]Helmsman:[/green] Sir! Forward {POSITION}, Depth {DEPTH}, Sir!")
+print(f"[green]Captain:[/green] Thank you, sailor. Yeoman, record that as {POSITION * DEPTH}!")
